ecb07dc499fdf8de7e42301c732afcc5df0438d0
Martin W <martin_wellard@comcast.com>
2024-04-27 14:28:09 -0400

    Add debug output to action script

--- action.sh
+++ action.sh
@@ -7,7 +7,7 @@ cat << EOH
 github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
 github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
 github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
-EOM >${SSH_HOME}/known_hosts
+EOH >${SSH_HOME}/known_hosts
 
 SSH_AGENT_ARGS=""
 
@@ -19,3 +19,6 @@ fi
 TMP_FILE=$( mktemp )
 ssh-agent ${SSH_AGENT_ARGS} >${TMP_FILE}
 source ${TMP_FILE}
+set
+ls -l ${SSH_HOME}
+cat ${SSH_HOME}/known_hosts
