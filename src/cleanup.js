ea206dda17f83f250c4eaeca57fb3ea21e03094d
Martin W <martin_wellard@comcast.com>
2024-06-25 13:32:19 -0400

    Add extr scripts and workflow

---
+++ src/cleanup.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core')
+const { execFileSync } = require('child_process')
+const { sshAgentCmd } = require('./paths.js')
+
+try {
+  // Kill the started SSH agent
+  console.log('Stopping SSH agent')
+  execFileSync(sshAgentCmd, ['-k'], { stdio: 'inherit' })
+} catch (error) {
+  console.log(error.message)
+  console.log('Error stopping the SSH agent, proceeding anyway')
+}
---
+++ src/paths.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+module.exports =
+  process.env['OS'] != 'Windows_NT'
+    ? {
+        // Use getent() system call, since this is what ssh does; makes a difference in Docker-based
+        // Action runs, where $HOME is different from the pwent
+        homePath: os.userInfo().homedir,
+        sshAgentCmd: 'ssh-agent',
+        sshAddCmd: 'ssh-add',
+        gitCmd: 'git'
+      }
+    : {
+        // Assuming GitHub hosted `windows-*` runners for now
+        homePath: os.homedir(),
+        sshAgentCmd: 'c://progra~1//git//usr//bin//ssh-agent.exe',
+        sshAddCmd: 'c://progra~1//git//usr//bin//ssh-add.exe',
+        gitCmd: 'c://progra~1//git//bin//git.exe'
+      }
